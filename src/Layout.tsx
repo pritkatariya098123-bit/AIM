623e9a21214ad5bc479aa64e9dcfbe42f5fe8744
katariyaprit3-hub <katariyaprit3@gmail.com>
2026-02-19 16:33:08 +0530

    edit components

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,13 @@
 // import React from 'react'
+import { useTheme } from "./assets/themes/Themes"
 import Header from "./Component/Header"
 import Sidebar from "./Component/Sidebar"
 
+
 export default function Layout() {
+    const { isDark } = useTheme()
     return (
-        <div className="min-h-screen w-screen bg-gray-900">
+        <div className={`${isDark ? 'bg-gray-800' : 'bg-gray-50'} min-h-screen w-screen`}>
             <Header />
             <Sidebar />
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "./assets/themes/Themes";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-)
+  </ThemeProvider>
+);
